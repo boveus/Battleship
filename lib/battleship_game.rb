91e197b843a42afb28a300d5e4957777f90ef9d7
Brandon Stewart <boveus@gmail.com>
2017-07-13 16:56:36 -0600

    Add seond menu.

--- lib/battleship_game.rb
+++ lib/battleship_game.rb
@@ -5,4 +5,14 @@ class BattleshipGame
   def main_menu
     "Welcome to BATTLESHIP\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
   end
+
+  def play
+    play_prompt_ln1 = "I have laid out my ships on the grid."
+    play_prompt_ln2 = "\nYou now need to layout your two ships."
+    play_prompt_ln3 = "\nThe first is two units long and the"
+    play_prompt_ln4 = "\nsecond is three units long."
+    play_prompt_ln5 = "\nThe grid has A1 at the top left and D4 at the bottom right."
+    play_prompt_ln6 = "\nEnter the squares for the two-unit ship: "
+    play_prompt_ln1 + play_prompt_ln2 + play_prompt_ln3 + play_prompt_ln4 + play_prompt_ln5 + play_prompt_ln6
+  end
 end
--- test/battleship_game_test.rb
+++ test/battleship_game_test.rb
@@ -16,6 +16,16 @@ class BattleshipGameTest < Minitest::Test
   end
 
   def test_main_menu_play
+    play_prompt_ln1 = "I have laid out my ships on the grid."
+    play_prompt_ln2 = "\nYou now need to layout your two ships."
+    play_prompt_ln3 = "\nThe first is two units long and the"
+    play_prompt_ln4 = "\nsecond is three units long."
+    play_prompt_ln5 = "\nThe grid has A1 at the top left and D4 at the bottom right."
+    play_prompt_ln6 = "\nEnter the squares for the two-unit ship: "
+
+    play_prompt = play_prompt_ln1 + play_prompt_ln2 + play_prompt_ln3 + play_prompt_ln4 + play_prompt_ln5 + play_prompt_ln6
+
+    assert_equal play_prompt, @game.play
   end
 
   def test_main_menu_instructions
