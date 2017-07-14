fd2f53a9a8a318294e898a9d3b29d0e2469bf2d9
Brandon Stewart <boveus@gmail.com>
2017-07-14 10:53:19 -0600

    Add map that can display ships that are on fire.

--- test/map_test.rb
+++ test/map_test.rb
@@ -20,7 +20,6 @@ class MapTest < Minitest::Test
     line5 = "D #{wave} #{wave} #{wave} #{wave}\n"
     bottom_border = "==========="
     map.render_lines
-
     map_display = border + line1 + line2 + line3 + line4 + line5 + bottom_border
     assert_equal map_display, map.display
   end
@@ -38,6 +37,26 @@ class MapTest < Minitest::Test
     bottom_border = "==========="
     map.a_grid[1].set_icon('ship')
     map.a_grid[2].set_icon('ship')
+    map.render_lines
+    map_display = border + line1 + line2 + line3 + line4 + line5 + bottom_border
+
+    assert_equal map_display, map.display
+  end
+
+  def test_map_can_display_ships_that_are_on_fire
+    map = Map.new
+    wave = `echo "\xF0\x9F\x8C\x8A"`.strip
+    ship = `echo "\xF0\x9F\x8E\xAB"`.strip
+    fire = `echo "\xF0\x9F\x94\xA5"`.strip
+    border = "===========\n"
+    line1 = ". 1 2 3 4 \n"
+    line2 = "A #{wave} #{ship} #{fire} #{wave}\n"
+    line3 = "B #{wave} #{wave} #{wave} #{wave}\n"
+    line4 = "C #{wave} #{wave} #{wave} #{wave}\n"
+    line5 = "D #{wave} #{wave} #{wave} #{wave}\n"
+    bottom_border = "==========="
+    map.a_grid[1].set_icon('ship')
+    map.a_grid[2].set_icon('fire')
     map.render_lines
     map_display = border + line1 + line2 + line3 + line4 + line5 + bottom_border
 
