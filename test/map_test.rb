1ae929e0fd26d7eee011beffd8a97758bf4737e6
Brandon Stewart <boveus@gmail.com>
2017-07-13 20:14:19 -0600

    Add wave art to map.

--- test/map_test.rb
+++ test/map_test.rb
@@ -10,12 +10,13 @@ class MapTest < Minitest::Test
 
 def test_map_can_display
   map = Map.new
+  wave = `echo "\xF0\x9F\x8C\x8A"`.strip
   border = "===========\n"
   line1 = ". 1 2 3 4 \n"
-  line2 = "A\n"
-  line3 = "B\n"
-  line4 = "C\n"
-  line5 = "D\n"
+  line2 = "A #{wave} #{wave} #{wave} #{wave}\n"
+  line3 = "B #{wave} #{wave} #{wave} #{wave}\n"
+  line4 = "C #{wave} #{wave} #{wave} #{wave}\n"
+  line5 = "D #{wave} #{wave} #{wave} #{wave}\n"
   bottom_border = "==========="
 
   map_display = border + line1 + line2 + line3 + line4 + line5 + bottom_border
